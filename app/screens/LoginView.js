52d8b4e8c89d721877d7641831165eaa298a5b50
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-11-11 10:10:42 -0500

    improves LoginView component

--- app/screens/LoginView.js
+++ app/screens/LoginView.js
@@ -52,4 +52,178 @@ class Login extends Component {
         .catch(onFailure);
     }
 
+    getNonFieldErrorMsg() {
+        let message = null;
+        const { errors } = this.state;
+        if (errors.non_field_errors) {
+            message = (
+                <View style={styles.errorMessageContainerStyle}>
+                    { errors.non_field_errors.map(item => (
+                        <Text style={styles.errorMessageTextStyle} key={item}>
+                            {item}
+                        </Text>
+                    ))}
+                </View>
+            );
+        }
+        return message;
+    }
+
+    getErrorMsgByField(field) {
+        let message = null;
+        if (this.state.errors[field]) {
+            message = (
+                <View style={styles.errorMessageContainerStyle}>
+                    {this.state.errors[field].map(item => (
+                        <Text style={styles.errorMessageTextStyle} key={item}>
+                            {item}
+                        </Text>
+                    ))}
+                </View>
+            );
+        }
+        return message;
+    }
+
+    render() {
+        const { isLoading } = this.state;
+
+        return (
+            <View style={styles.containerStyle}>
+                <Spinner visible={isLoading} />
+
+                { !this.state.isAuthorized ? 
+                    <View>
+                        <View style={styles.logotypeContainer}>
+                            <Image
+                                source={require("")}
+                                style={styles.logotype}
+                            />
+                        </View>
+                        <TextInput
+                            style={styles.input}
+                            value={this.state.username}
+                            maxLength={256}
+                            placeholder="Enter username..."
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            returnKeyType="next"
+                            onSubmitEditing={event =>
+                            this.passwordInput.wrappedInstance.focus()
+                            }
+                            onChangeText={this.onUsernameChange}
+                            underlineColorAndroid="transparent"
+                            placeholderTextColor="#999"
+                        />
+
+                        {this.getErrorMessageByField('username')}
+
+                        <TextInput
+                            ref={node => {
+                            this.passwordInput = node;
+                            }}
+                            style={styles.input}
+                            value={this.state.password}
+                            maxLength={40}
+                            placeholder="Enter password..."
+                            onChangeText={this.onPasswordChange}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            returnKeyType="done"
+                            blurOnSubmit
+                            onSubmitEditing={this.onPressLogin.bind(this)}
+                            secureTextEntry
+                            underlineColorAndroid="transparent"
+                            placeholderTextColor="#999"
+                        />
+
+                        {this.getErrorMessageByField('password')}
+
+                        {this.getNonFieldErrorMessage()}
+
+                        <TouchableOpacity
+                            style={styles.loginButton}
+                            onPress={this.onPressLogin.bind(this)}>
+                            <Text style={styles.loginButtonText}>LOGIN</Text>
+                        </TouchableOpacity>
+                    </View> 
+                    : 
+                    <View>
+                        <Text>Successfully authorized!</Text>
+                    </View>
+                }
+            </View>
+        );
+    }
 }
+
+const utils = {
+    colors: {primaryColor: '#af0e66'},
+    dimensions: {defaultPadding: 12},
+    fonts: {
+        largeFontSize: 18,
+        mediumFontSize: 16,
+        smallFontSize: 12
+    }
+}
+
+const styles = {
+    innerContainer: {
+      marginBottom: 32,
+    },
+    logotypeContainer: {
+      alignItems: 'center',
+    },
+    logotype: {
+      maxWidth: 280,
+      maxHeight: 100,
+      resizeMode: 'contain',
+      alignItems: 'center',
+    },
+    containerStyle: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#f6f6f6',
+    },
+    input: {
+      height: 50,
+      padding: 12,
+      backgroundColor: 'white',
+      borderRadius: 6,
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 4,
+      },
+      shadowOpacity: 0.05,
+      shadowRadius: 4,
+      marginBottom: utils.dimensions.defaultPadding,
+    },
+    loginButton: {
+      borderColor: utils.colors.primaryColor,
+      borderWidth: 2,
+      padding: utils.dimensions.defaultPadding,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 6,
+    },
+    loginButtonText: {
+      color: utils.colors.primaryColor,
+      fontSize: utils.fonts.mediumFontSize,
+      fontWeight: 'bold',
+    },
+    errorMessageContainerStyle: {
+      marginBottom: 8,
+      backgroundColor: '#fee8e6',
+      padding: 8,
+      borderRadius: 4,
+    },
+    errorMessageTextStyle: {
+      color: '#db2828',
+      textAlign: 'center',
+      fontSize: 12,
+    },
+  };
+  
+  export default Login;
